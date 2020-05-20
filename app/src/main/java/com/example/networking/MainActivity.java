0c326468fa460f967c7791f542d3eb1d84d5e677
abhishekjaiswal3158 <abhishekjaiswal3158@gmail.com>
2020-05-20 18:16:25 +0530

    ACTION BUTTON AND ABOUT IS ADDED..

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -28,14 +31,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button glo=(Button)findViewById(R.id.global);
-        glo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Global_Activity.class);
-                startActivity(intent);
-            }
-        });
+
+
 
         ListView earthquakeListView = (ListView) findViewById(R.id.listView);
         final ArrayList<Corona> arrayList=new ArrayList<Corona>();
@@ -97,5 +94,36 @@ public class MainActivity extends AppCompatActivity {
                 mAdapter.addAll(data);
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.globaloption:
+                Intent intent = new Intent(MainActivity.this, Global_Activity.class);
+                startActivity(intent);
+                break;
+
+
+
+            case R.id.about:
+                Intent intent2 = new Intent(MainActivity.this, about_activity.class);
+                startActivity(intent2);
+
+                break;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+        return true;
     }
 }
